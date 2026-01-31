ccb8e38fd18cb69b5e596277f16c9dabc58e8977
João Koehler <62446005+joaokoehlerz@users.noreply.github.com>
2026-01-31 15:58:57 -0300

    hazard
    hazard

--- scripts/hazard.gd
+++ scripts/hazard.gd
@@ -1,25 +1,32 @@
 extends RigidBody2D
 
-@export var fall_delay: float
+@export var hazard_id: String = "A"
+@export var fall_delay: float = 1.0
 
-# Variável para garantir que ele só suma SE já estiver caindo (opcional, mas recomendado)
-var is_falling: bool = false 
+var is_falling = false
+var activated = false
 
 func _ready():
-	# ATENÇÃO: Ativa o monitoramento via código para garantir que funcione
 	contact_monitor = true
 	max_contacts_reported = 1
-	
 	body_entered.connect(_on_body_entered)
-	gravity_scale = 0
-	
-	var timer = get_tree().create_timer(fall_delay)
-	await timer.timeout
-	
-	gravity_scale = 1
-	is_falling = true # Agora ele pode ser destruído
 
-func _on_body_entered(_body):
-	# Se quiser que suma ao bater em QUALQUER coisa:
-	if is_falling: # Essa checagem evita que ele suma se encostar no teto antes de cair
+	gravity_scale = 0
+	add_to_group("hazard_" + hazard_id)
+
+func activate():
+	if activated:
+		return
+	activated = true
+
+	await get_tree().create_timer(fall_delay).timeout
+	gravity_scale = 1
+	sleeping = false
+	is_falling = true
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		body.dead = true
+
+	if is_falling:
 		queue_free()

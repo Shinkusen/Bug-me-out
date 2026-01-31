4d2ffaa2982aaa9bfa2b3975925292b67e87b5f1
Felipe de Aragão Falcão <felipefalcao2910@gmail.com>
2026-01-31 16:22:04 -0300

    estalactite

--- scripts/hazard.gd
+++ scripts/hazard.gd
@@ -1,32 +1,20 @@
 extends RigidBody2D
 
-@export var hazard_id: String = "A"
-@export var fall_delay: float = 1.0
-
-var is_falling = false
-var activated = false
-
 func _ready():
-	contact_monitor = true
-	max_contacts_reported = 1
-	body_entered.connect(_on_body_entered)
-
 	gravity_scale = 0
-	add_to_group("hazard_" + hazard_id)
 
-func activate():
-	if activated:
-		return
-	activated = true
+# --- SINAL DO FILHO (Area2D) ---
+func _on_area_2d_body_entered(body):
+	if body.name == "Player":
+		gravity_scale = 1
+		
+		if has_node("Area2D"):
+			$Area2D.queue_free()
 
-	await get_tree().create_timer(fall_delay).timeout
-	gravity_scale = 1
-	sleeping = false
-	is_falling = true
-
+# --- SINAL DO PAI (RigidBody) ---
 func _on_body_entered(body):
 	if body.name == "Player":
-		body.dead = true
-
-	if is_falling:
-		queue_free()
+		pass
+		# Dano
+	
+	queue_free()

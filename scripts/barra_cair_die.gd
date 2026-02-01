752cc793db1a4eeffea480dea787df6dd102024a
Felipe de Aragão Falcão <felipefalcao2910@gmail.com>
2026-02-01 04:54:12 -0300

    quase fim dnv

--- scripts/barra_cair_die.gd
+++ scripts/barra_cair_die.gd
@@ -3,3 +3,6 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		GameController.player.dead = true
+	elif body.name == "Bloco":
+		$Bloco_unico.visible = true
+		$Bloco.visible = false
